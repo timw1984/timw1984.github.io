9b09b9de277d13102a3621ca28960a5011f688de
timw1984 <timw1984@gmail.com>
2015-05-26 20:20:58 -0400

    v1.2

--- script.js
+++ script.js
@@ -2,9 +2,6 @@
 $(document).ready(function() {
     $("#notready").fadeOut(5000);
 	$('.bxslider').bxSlider({
-		  auto: true,
-		  autoControls: true,
-          pause: 4000,
           slideMargin: 20,
 		  captions: true
 	
